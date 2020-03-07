9e92ac0cdf403fc4e465baa625347e6954a45a85
msk <54884351+minsangkim89@users.noreply.github.com>
2020-03-06 23:41:48 -0800

    update build script

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
+pushd %~dp0
 del /q dist\*
 echo Build with pyinstaller...
-pyinstaller ticker.py --onefile -n iidxseg --add-data "DSEG14Classic-Italic.ttf;." --noconsole
+pyinstaller ticker.py --onefile -n iidxseg --add-data "DSEG14Classic-Italic.ttf;."
 echo Create an archive...
 powershell Compress-Archive -Path dist\* -DestinationPath dist\iidxseg_release.zip
+popd
